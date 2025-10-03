f3c23bc27e3ffc97ff91be5dbe4e5733a266331e
gabriel D <gabriel.fanchiotti@hotmail.com>
2025-10-03 12:53:51 -0300

    comeco do menu

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
 
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import entidades.Movimentacao;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //fazer o menu
 
+        while (true) {
+
+    int opcao;
+        switch (opcao) {
+            case 1:
+                break;
+            default:
+                System.out.println("numero invalido, favor inserir um numero dentro das opcoes do sistema");
+        }
+
+            }
         }
     }
