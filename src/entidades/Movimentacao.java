0e47ea4aaa69efa5faba03d1eeccd1dbe7d7a958
Gabriel Dazilio Mac <gabriel.fanchiotti@hotmail.com>
2025-10-07 18:40:19 -0300

    classe Produtos feita

--- src/entidades/Movimentacao.java
+++ src/entidades/Movimentacao.java
@@ -1,4 +1,12 @@
 package entidades;
 
 public class Movimentacao {
+
+    /*
+
+
+
+
+    */
+
 }
--- src/entidades/Produtos.java
+++ src/entidades/Produtos.java
@@ -1,4 +1,59 @@
 package entidades;
 
 public class Produtos {
+
+    private int idProduto;
+    private String nome;
+    private String descricao;
+    private int estoque;
+
+
+    public Produtos(int idProduto, String nome, String descricao, int estoque) {
+        this.idProduto = idProduto;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.estoque = estoque;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    @Override
+    public String toString() {
+        return "Produtos{" +
+                "idProduto=" + idProduto +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", estoque=" + estoque +
+                '}';
+    }
 }
