7c9aad42c0b08a612db6b8987e1c9a07a3a0fa29
Gabriel Dazilio Mac <gabriel.fanchiotti@hotmail.com>
2025-10-08 17:54:23 -0300

    splash screen praticamente pronta

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,66 @@
-
-
-import entidades.Movimentacao;
-import entidades.Produtos;
 import java.util.Scanner;
 
+import dao.movimentacaoDAO;
+import dao.ProdutosDAO;
+
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Movimentacao m = new Movimentacao();
-        Produtos p = new Produtos();
+// splash screen
+        System.out.println("##################################################");
+        System.out.println("#                                                #");
+        System.out.println("#        SISTEMA DE CONTROLE DE ESTOQUE          #");
+        System.out.println("#                                                #");
+        System.out.println("#      TOTAL DE REGISTROS EXISTENTES             #");
+        System.out.printf("#  1 - PRODUTOS:      %5d                    #%n", //totalProdutos);
+// System.out.printf("#  2 - MOVIMENTAÇÕES: %5d                    #%n", totalMovimentacoes);
+                System.out.println("#                                                #"); 
+        System.out.println("#      CRIADO POR: GABRIEL DAZILIO               #");
+        System.out.println("#                  VICTOR CASTRO                 #");
+        System.out.println("#                                                #");
+        System.out.println("#      DISCIPLINA: BANCO DE DADOS                #");
+        System.out.println("#      PROFESSOR: HOWARD ROATTI                  #");
+        System.out.println("#                                                #");
+        System.out.println("##################################################\n");
 
+        // loop
         int opcao = -1;
-
         while (opcao != 0) {
-            System.out.println("\n=== MENU ===");
-            System.out.println("1 - Relatorios 1");
-            System.out.println("2 - Inserir Registro 2");
-            System.out.println("3 - Remover Registro 3");
-            System.out.println("4 - Atualizar Registro 4");
+
+            System.out.println("\n--- MENU PRINCIPAL ---");
+            System.out.println("1 - Relatórios");
+            System.out.println("2 - Inserir Registro");
+            System.out.println("3 - Remover Registro");
+            System.out.println("4 - Atualizar Registro");
             System.out.println("0 - Sair");
-            System.out.print("Escolha: ");
-
-            opcao = scanner.nextInt();
-
+            System.out.print("Escolha uma opção: ");
 
             switch (opcao) {
-                case 0:
-                    System.out.println("Voce esta saindo do programa"
-                            + "\nObrigado por usar nosso programar");
-                    break;
                 case 1:
+                    System.out.println("voce escolheu: relatorios");
+
                     break;
                 case 2:
+                    System.out.println("voce escolheu: inserir registro");
                     break;
                 case 3:
+                    System.out.println("voce escolheu: remover registro");
+
                     break;
                 case 4:
+                    System.out.println("voce escolheu: atualizar registro");
                     break;
-                case 5:
+                case 0:
+                    System.out.println("saindo do sistema");
                     break;
-
                 default:
-                    System.out.println("Numero invalido, favor inserir um numero dentro das opcoes do sistema");
+                    System.out.println("opção inválida, por favor, escolha uma opção do menu.");
+                    break;
             }
-
         }
+
+        scanner.close();
     }
 }
