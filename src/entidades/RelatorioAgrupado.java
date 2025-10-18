24459240bb62c90a246a07b8a498423c593d3f5c
Gabriel Dazilio Mac <gabriel.fanchiotti@hotmail.com>
2025-10-18 15:39:27 -0300

    inicio da classe RelatoriosAgrupados

--- src/entidades/RelatorioAgrupado.java
+++ src/entidades/RelatorioAgrupado.java
@@ -6,4 +6,25 @@ public class RelatorioAgrupado {
     private String tipoMovimentacao;
     private int totalQuantidade;
 
+    public RelatorioAgrupado() {
+
+    }
+
+
+    public String getTipoMovimentacao() {
+        return tipoMovimentacao;
+    }
+
+    public void setTipoMovimentacao(String tipoMovimentacao) {
+        this.tipoMovimentacao = tipoMovimentacao;
+    }
+
+
+    public int getTotalQuantidade() {
+        return totalQuantidade;
+    }
+
+    public void setTotalQuantidade(int totalQuantidade) {
+        this.totalQuantidade = totalQuantidade;
+    }
 }
