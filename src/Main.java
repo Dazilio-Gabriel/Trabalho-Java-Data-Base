10dd0c698e364b2bc3a23fc9225d849047dc8b42
Gabriel Dazilio Mac <gabriel.fanchiotti@hotmail.com>
2025-10-20 21:16:10 -0300

    readme feito

--- src/Main.java
+++ src/Main.java
@@ -105,7 +105,6 @@ public class Main {
                                 }
                                 System.out.println("---------------------------------------");
 
-
                                 break;
 
                             case 0:
